18c725917c7f7e6be94a557e69ca8272a440d97a
Shelwin Sunga <shelwinsunga@gmail.com>
2022-09-26 16:19:18 -0500

    trimming

--- theme.config.js
+++ theme.config.js
@@ -6,7 +6,7 @@ export default {
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Nintern</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        Docs Demo
+        Terminology
       </span>
     </>
   ),
